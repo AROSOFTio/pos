1f98a52c374be5651b658007bf82cbe6ae3cd5ca
AROSOFT Innovations Ltd <arosoftio@gmail.com>
2026-09-19 15:21:51 +0300

    Register PWA and render unobtrusive install prompt

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,7 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
+import PWAInstallPrompt from './components/PWAInstallPrompt'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode><App /></StrictMode>,
+  <StrictMode><><App /><PWAInstallPrompt /></></StrictMode>,
 )
+
+
+if('serviceWorker' in navigator){
+  window.addEventListener('load',()=>navigator.serviceWorker.register('/sw.js').catch(()=>{}))
+}
