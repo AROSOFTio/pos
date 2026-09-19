6ffd498d93c79f707e2ef3558a6fcbf77b3aba8d
AROSOFT Innovations Ltd <arosoftio@gmail.com>
2026-09-19 04:52:05 +0300

    fix: import reports navigation icon

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { BarChart3, useEffect, useState } from 'react'
 import { ArrowLeft, ArrowRight, Banknote, Bell, Boxes, ChefHat, ClipboardList, Eye, EyeOff, LayoutDashboard, LockKeyhole, LogOut, Mail, Menu as MenuIcon, Package, ReceiptText, Search, Settings as SettingsIcon, ShieldCheck, ShoppingCart, Truck, UserRound, UsersRound, UtensilsCrossed, X, Building2 } from 'lucide-react'
 import { api, nice, type User } from './api'
 import { MauzoLogo } from './Brand'
