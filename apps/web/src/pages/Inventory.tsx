d690c8911f6c8ad439bdde16789f8ee4c67637f3
AROSOFT Innovations Ltd <arosoftio@gmail.com>
2026-09-20 04:44:53 +0300

    Fix inventory build imports

--- apps/web/src/pages/Inventory.tsx
+++ apps/web/src/pages/Inventory.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useMemo, useState } from 'react'
-import { AlertTriangle, Boxes, Building2, ClipboardList, RefreshCw, Search, PackageCheck, CookingPot, Plus } from 'lucide-react'
+import { AlertTriangle, Boxes, Building2, ClipboardList, RefreshCw, Search, Plus } from 'lucide-react'
 import { api, money, nice } from '../api'
 import { PageHeading, Stat, Panel, DataTable, Loading, Badge, Modal } from '../components'
 
